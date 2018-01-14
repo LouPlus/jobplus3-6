0bf08ddef374c78229977b5ed71b8fefd0b024de
clikks <anycosmo@qq.com>
2018-01-14 23:15:15 +0800

    增加开发配置类的内容

--- jobplus/config.py
+++ jobplus/config.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
+
 class BaseConfig(object):
     SECRET_KEY = 'makesure to set a very secret key'
 
+    
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqldb://root@localhost:3306/jobplus?charset=utf8'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TEMPLATES_AUTO_RELOAD = True
 
-
+    
 class ProductionConfig(BaseConfig):
-    pass
+    DEBUG = False
 
 
 class TestingConfig(BaseConfig):
