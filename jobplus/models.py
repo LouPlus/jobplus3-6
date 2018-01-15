614efb30a26a72a858f180475017ebd3497aea6a
lvix <iamliufu@gmail.com>
2018-01-16 07:59:42 +0800

    fix #12, fix #13, fix #14, fix #15 completed and tested db models

--- jobplus/models.py
+++ jobplus/models.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 
-from jobplus import app
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 
 class Base(db.Model):
@@ -17,7 +16,6 @@ class Base(db.Model):
 
 
 class User(Base):
-
     __tablename__ = 'user'
 
     ROLE_ADMIN = 10
@@ -27,7 +25,7 @@ class User(Base):
     STATUS_NORMAL = 0
     STATUS_SUSPENDED = -1
 
-    id = db.Column(db.Integer, primarykey=True)
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), unique=True, index=True, nullable=False)
     email = db.Column(db.String(64), unique=True, index=True, nullable=False)
     _password = db.Column('password', db.String(256), nullable=False)
@@ -35,11 +33,13 @@ class User(Base):
     status = db.Column(db.SmallInteger, default=STATUS_NORMAL)
 
     # 需要修改
-    seekers = db.relationship('Seeker', uselist=False, back_populates="user")
-    companies = db.relationship('Company', uselist=False, back_populates="user")
+    seeker_info = db.relationship('Seeker', uselist=False, backref='user')
+    company_info = db.relationship('Company', uselist=False, backref='user')
+    jobs = db.relationship('Job', uselist=True, backref='user')
+    resumes = db.relationship('Resume', uselist=True, backref='user')
 
     def __repr__(self):
-        return '<User: {}'.format(self.username)
+        return '<User {}: {}>'.format(self.id, self.username)
 
     @property
     def password(self):
@@ -66,7 +66,6 @@ class User(Base):
 
 
 class Seeker(Base):
-
     __tablename__ = 'seeker'
 
     # 默认头像的url
@@ -81,11 +80,12 @@ class Seeker(Base):
     EDU_PHD = 40
     EDU_OTHER = 50
 
-    id = db.Column(db.Integer, primarykey=True)
+    id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
 
-    phone = db.Column(db.String(11), nullable=False)
     gender = db.Column(db.SmallInteger, default=GENDER_M, nullable=False)
+
+    phone = db.Column(db.String(11), nullable=False)
 
     # 真实姓名
     name = db.Column(db.String(32), nullable=False)
@@ -95,17 +95,46 @@ class Seeker(Base):
     college = db.Column(db.String(128))
     # 学历
     education = db.Column(db.SmallInteger, default=10, nullable=False)
-    # 工作年限(0~11)
-    working_age = db.Column(db.SmallInteger, default=0, nullable=False)
+    # 专业
+    major = db.Column(db.String(64))
+    # 工作年限：0~11，11表示十年以上
+    service_year = db.Column(db.SmallInteger, default=0, nullable=False)
+
+    def __repr__(self):
+        return '<Seeker(id={}, name={})>'.format(self.id, self.name)
+
+
+# 简历表使用 MongoDB, 但暂时写个MySQL原型
+class Resume(Base):
+    __tablename__ = 'resume'
+
+    TYPE_WEB_RESUME = 1
+    TYPE_FILE_RESUME = 2
+
+    # 性别
+    GENDER_M = 10
+    GENDER_F = 20
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
+    resume_type = db.Column(db.SmallInteger, default=TYPE_WEB_RESUME, nullable=False)
+
+    photo = db.Column(db.String(256))
+    expect_salary_min = db.Column(db.Integer)
+    expect_salary_max = db.Column(db.Integer)
+
+    edu_exp = db.Column(db.Text)
+    self_intro = db.Column(db.Text)
+    project_exp = db.Column(db.Text)
+    expect_job = db.Column(db.String(64))
+    attachment = db.Column(db.String(256))
 
     # relationship
-
-# 简历表使用 MongoDB
-# 此处暂时不写
+    def __repr__(self):
+        return '<Seeker(id={})>'.format(self.id)
 
 
 class Company(Base):
-
     __tablename__ = 'company'
 
     # 如果不上传Logo
@@ -117,7 +146,7 @@ class Company(Base):
     SCALE_150_500 = 3
     SCALE_500_MORE = 4
 
-    id = db.Column(db.Integer, primarykey=True)
+    id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
 
     name = db.Column(db.String(128), unique=True, nullable=False, index=True)
@@ -135,8 +164,19 @@ class Company(Base):
     manager_photo = db.Column(db.String(256))
 
 
-class Job(Base):
+STATUS_SENT = 1
+STATUS_CHECKED = 2
+STATUS_ACCEPTED = 3
+STATUS_REJECTED = 4
 
+Mailing = db.Table('mailing',
+                   db.Column('resume_id', db.Integer, db.ForeignKey('resume.id'), primary_key=True),
+                   db.Column('job_id', db.Integer, db.ForeignKey('job.id'), primary_key=True),
+                   db.Column('status', db.SmallInteger, default=STATUS_SENT, nullable=False)
+                   )
+
+
+class Job(Base):
     __tablename__ = 'job'
 
     # 受教育程度
@@ -146,31 +186,16 @@ class Job(Base):
     EDU_PHD = 40
     EDU_NO_LIMIT = 60
 
-    id = db.Column(db.Integer, primarykey=True)
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), index=True, nullable=False)
     company_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete='CASCADE'))
     salary_min = db.Column(db.Integer, default=0)
     salary_max = db.Column(db.Integer, default=0)
-    exp_required_min = db.Column(db.Integer, default=0)
-    exp_required_max = db.Column(db.Integer, default=0, nullable=True)
+    exp_required = db.Column(db.Integer, default=0)
     edu_required = db.Column(db.SmallInteger, default=EDU_NO_LIMIT, nullable=False)
-    is_full_time = db.Column(db.Boolean, deafult=True, nullable=False)
+    is_full_time = db.Column(db.Boolean, default=True, nullable=False)
     description = db.Column(db.Text, nullable=False)
     work_address = db.Column(db.String(512), nullable=False)
 
-
-class Mailing(Base):
-
-    __tablename__ = 'mailing'
-
-    STATUS_SENT = 1
-    STATUS_CHECKED = 2
-    STATUS_ACCEPTED = 3
-    STATUS_REJECTED = 4
-
     # relationship
-    user_id = None
-    resume_id = None
-
-    status = db.Column(db.SmallInteger, default=STATUS_SENT, nullable=False)
-
-
+    resumes = db.relationship('Resume', secondary=Mailing, backref=db.backref('jobs'))
