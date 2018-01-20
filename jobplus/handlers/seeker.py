24e3e183e951287aedefd1d3343ac11c3195f06c
lvix <iamliufu@gmail.com>
2018-01-21 04:24:47 +0800

    修正用户注册，用户信息页面表单的一些bug，优化页面显示

--- jobplus/handlers/seeker.py
+++ jobplus/handlers/seeker.py
@@ -3,15 +3,21 @@
 
 
 from flask import Blueprint, render_template, flash, url_for, redirect
+from flask_login import current_user, login_user
 from jobplus.forms import UserinfoForm, SeekerRegisterForm
-
+from jobplus.models import Seeker
 
 seeker = Blueprint('seeker', __name__, url_prefix='/user')
 
 
 @seeker.route('/profile', methods=['GET', 'POST'])
 def profile():
+    if not current_user.is_seeker:
+        return redirect(url_for('front.index'))
     form = UserinfoForm()
+    form.load_seeker_info()
+    if not Seeker.query.filter_by(user_id=current_user.id).first():
+        flash('请完善个人信息', 'success')
     if form.validate_on_submit():
         form.update_user()
         flash('更新成功', 'success')
@@ -21,9 +27,17 @@ def profile():
 @seeker.route('/register', methods=['GET', 'POST'])
 def register():
     form = SeekerRegisterForm()
+    if current_user.is_authenticated:
+        # 如果用户已登录，并尝试打开注册页面，会被跳转到profile
+        if current_user.is_seeker():
+            return redirect(url_for('.profile'))
+        else:
+            return redirect(url_for('front.index'))
     if form.validate_on_submit():
-        form.create_user()
-        flash('注册成功，请登录！', 'success')
+        user = form.create_user()
+        # 注册后登录
+        login_user(user, True)
+        flash('注册成功，请完善个人信息', 'success')
         return redirect(url_for('.profile'))
     return render_template('seeker_register.html', form=form)
 
