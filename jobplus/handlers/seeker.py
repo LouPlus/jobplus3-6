767f86dfcc3ff424b9f737f1d5f0f411e9253711
Alex961120 <805119233@qq.com>
2018-01-20 23:08:35 +0800

    fixed #23 求职者注册和个人信息配置页 (#29)
    * update
    
    * fixed #23 求职者注册和个人信息配置页
    
    * Update forms.py

--- jobplus/handlers/seeker.py
+++ jobplus/handlers/seeker.py
@@ -2,8 +2,28 @@
 # encoding: utf-8
 
 
-from flask import Blueprint
+from flask import Blueprint, render_template, flash, url_for, redirect
+from jobplus.forms import UserinfoForm, SeekerRegisterForm
 
 
-seeker = Blueprint('seeker', __name__, url_prefix='/seeker')
+seeker = Blueprint('seeker', __name__, url_prefix='/user')
+
+
+@seeker.route('/profile', methods=['GET', 'POST'])
+def profile():
+    form = UserinfoForm()
+    if form.validate_on_submit():
+        form.update_user()
+        flash('更新成功', 'success')
+    return render_template('user.html', form=form)
+
+
+@seeker.route('/register', methods=['GET', 'POST'])
+def register():
+    form = SeekerRegisterForm()
+    if form.validate_on_submit():
+        form.create_user()
+        flash('注册成功，请登录！', 'success')
+        return redirect(url_for('.profile'))
+    return render_template('seeker_register.html', form=form)
 
