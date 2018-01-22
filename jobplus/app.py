534059847bfbe621944530e66700d80ff17f49d6
Lvix Lf <iamliufu@gmail.com>
2018-01-22 16:08:25 +0800

    fixed 导航栏添加企业注册链接，优化时间显示 #33 (#38)
    * fix #33 导航栏添加企业注册链接，优化时间显示

--- jobplus/app.py
+++ jobplus/app.py
@@ -11,6 +11,27 @@ from jobplus.config import configs
 from jobplus.models import db, User
 from jobplus.forms import photos
 
+import datetime
+
+
+def register_filters(app):
+
+    @app.template_filter()
+    def timesince(value):
+        now = datetime.datetime.utcnow()
+        delta = now - value
+        if delta.days > 365:
+            return '{}年前'.format(delta.days // 365)
+        if delta.days > 30:
+            return '{}月前'.format(delta.days//30)
+        if delta.days > 0:
+            return '{}天前'.format(delta.days)
+        if delta.seconds > 3600:
+            return '{}小时前'.format(delta.seconds // 3600)
+        if delta.seconds > 60:
+            return '{}分钟前'.format(delta.seconds // 60)
+        return '刚刚'
+
 
 def register_blueprints(app):
     from .handlers import front, company, job, seeker, admin
@@ -46,6 +67,7 @@ def create_app(config):
 
     register_blueprints(app)
     register_extensions(app)
+    register_filters(app)
     db.init_app(app)
     Migrate(app, db)
     configure_uploads(app, photos)
