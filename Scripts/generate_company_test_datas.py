a380449115fc55b7e60864bb3b115427d9f38050
clikks <anycosmo@qq.com>
2018-02-01 00:52:05 +0800

    增加爬虫，生成企业数据，修复bug

--- Scripts/generate_company_test_datas.py
+++ Scripts/generate_company_test_datas.py
@@ -1,48 +1,79 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
 import os
 import json
 from random import randint
 
 from faker import Faker
+
 from jobplus.models import db, User, Company
 
+
 fake = Faker(locale='zh-cn')
+email_set = set()
+
+with open(os.path.join(os.path.dirname(__file__), 'data/company_data.json')) as f:
+        company_detail = json.load(f)
 
 def iter_users():
-    with open(os.path.join(os.path.dirname(__file__),'jobplusspider/jobplusspider/spiders/company_data.json')) as f:
-        company_detail = json.load(f)
     for company in company_detail:
         name = company['name']
+        email = fake.email()
+        while email in email_set:
+            email = fake.email()
+        email_set.add(email)
         yield User(
             username = name,
-            email = fake.email(),
+            email = email,
             password = '123456',
             role = 30
         )
 
-def iter_company():
-    with open(os.path.join(os.path.dirname(__file__),'jobplusspider/jobplusspider/spiders/company_data.json')) as f:
-        company_detail = json.load(f)
-    with open(os.path.join(os.path.dirname(__file__),'jobplusspider/jobplusspider/spiders/manager_data.json')) as m:
-        manager_img = json.load(m)
+def get_photo():
+    path = os.path.join(os.path.dirname(__file__), 'data/manger_photo.json')
+    with open(path) as f:
+        manager_img = json.load(f)
+    for photo in manager_img:
+        yield photo['manager_photo']
+
+def iter_companys():
+    photo = get_photo()
+    
     for company in company_detail:
         name = company['name']
         user = User.query.filter_by(username=name).first()
-        for img in manager_img:
-            yield Company(
-                name=user.username,
-                logo = company['image_urls'],
-                web_url = company['web_url'],
-                found_date = fake.date(),
-                city = company['city'],
-                address = fake.address(),
-                scale = randint(0, 4),
-                industry = company['industry'],
-                email = fake.email(),
-                phone = fake.phone_number(),
-                manager_name = fake.name(),
-                manager_job = fake.job(),
-                manager_photo = img['manager_photo'],
-                slogan = company['slogan'],
-                products_display = fake.sentence(),
-                description = company['description']
+        phone = fake.phone_number()
+        yield Company(
+            user_id = user.id,
+            name = user.username,
+            logo = company['image_urls'],
+            web_url = company['web_url'],
+            found_date = fake.date(),
+            city = company['city'],
+            address = fake.address(),
+            scale = randint(0, 4),
+            industry = company['industry'],
+            email = fake.email(),
+            phone = phone,
+            fax = phone,
+            manager_name = fake.name(),
+            manager_job = fake.job(),
+            manager_photo = next(photo),
+            slogan = company['slogan'],
+            products_display = fake.sentence(),
+            description = company['description']
             )
+
+def run():
+    for user in iter_users():
+        db.session.add(user)
+
+    for company in iter_companys():
+        db.session.add(company)
+    
+    try:
+        db.session.commit()
+    except Exception as e:
+        print(e)
+        db.session.rollback()
